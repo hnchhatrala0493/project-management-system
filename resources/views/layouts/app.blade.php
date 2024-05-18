39c04f26f40318f2cc1f72ec50be181bba1ba838
Himanshu <hnchhatrala@gmail.com>
2024-05-18 12:59:48 +0530

    file path change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,16 +18,16 @@
     <meta property="og:url" content="https://keenthemes.com/metronic" />
     <meta property="og:site_name" content="Keenthemes | Metronic" />
     <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-    <link rel="shortcut icon" href="/admin/assets/media/logos/favicon.ico" />
+    <link rel="shortcut icon" href="../admin/assets/media/logos/favicon.ico" />
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
     <!--end::Fonts-->
     <!--begin::Vendor Stylesheets(used by this page)-->
-    <link href="/admin/assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../admin/assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
     <!--end::Vendor Stylesheets-->
     <!--begin::Global Stylesheets Bundle(used by all pages)-->
-    <link href="/admin/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="/admin/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../admin/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../admin/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
 </head>
 <!--end::Head-->
@@ -285,8 +285,8 @@
             </div>
             <!--end::Page-->
         </div>
-        <script src="/admin/assets/plugins/global/plugins.bundle.js"></script>
-        <script src="/admin/assets/js/scripts.bundle.js"></script>
+        <script src="../admin/assets/plugins/global/plugins.bundle.js"></script>
+        <script src="../admin/assets/js/scripts.bundle.js"></script>
 </body>
 <!--end::Body-->
 
